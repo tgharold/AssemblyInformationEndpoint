aa2bfaf49e35275ff12561ffc8bcc3b52d13b4c5
Thomas Harold <tgharold@gmail.com>
2022-09-02 08:20:45 -0400

    Create .NET Framework MVC example project

---
+++ examples/FrameworkMVCExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FrameworkMVCExample.Startup))]
+namespace FrameworkMVCExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
